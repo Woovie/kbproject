611e3a195db357d1b3e4bd1e85a46cca552ee842
Jordan Banasik <woovie@woovie.net>
2019-10-20 02:08:59 -0500

    Initial class build
    Testing my code, learning more about classes

--- scrapers/cms/shopify/main.py
+++ scrapers/cms/shopify/main.py
@@ -1,7 +1,19 @@
-import aiohttp, configparser, oembed
+import aiohttp, configparser, oembed, asyncio
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-class shopify()
+class shopify():
+    def __init__(self, url):
+        self.products = dict()
+        self.site = url
+        self.uri = config['shopify']['dataloc']
+        self.url = f"{self.site}{self.uri}"
 
+    async def crawl():
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url) as r:
+                print(r)
+
+shopify = shopify('https://dailyclack.com')
+asyncio.run(shopify.crawl())
