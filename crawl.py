78529f5a9e8991df9f51d0565fd75265a23bfd2e
Jordan Banasik <woovie@woovie.net>
2019-10-23 19:08:50 +0000

    Cleanup of old files

--- crawl.py
+++
@@ -1,3 +0,0 @@
-import aiohttp
-
-async def crawl(url):                                                                                         async with aiohttp.ClientSession() as session:                                                                async with session.get(url) as r:                                                                             if r.status == 200:                                                                                           return await r.text()                                                                                 else:                                                                                                         raise ResponseError(f"[ERROR] GET {url} returned unexpected value: {r.status} {r.reason}, expected 200 OK")  
