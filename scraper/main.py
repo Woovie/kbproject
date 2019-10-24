88feb755219db03b938b28befa5a41f8bb960a30
Jordan Banasik <woovie@woovie.net>
2019-10-24 18:54:52 +0000

    First logging commit doesn't work yet :(

--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #standard modules
-import json, asyncio
+import json, asyncio, logging, os, logging
 
 #My custom modules
 import baseVendors, baseCMS
@@ -13,6 +13,7 @@ vendors = []
 cms = []
 
 async def main():
+    logger.critical('test')
     global vendors
     global cms
     vendors = await baseVendors.loadVendors()
@@ -20,5 +21,13 @@ async def main():
     for vendor in vendors:
         ven = baseVendors.vendor(vendor['vendorName'], vendor['vendorURL'], vendor['cms'], vendor['scrape'])#name, url, cms, active
 
+logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s %(levelname)s %(filename)s %(message)s',
+                    datefmt='[%d-%m-%Y %H:%M:%S]',
+                    filename='debug.log',
+                    filemode='w')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 asyncio.run(main())
