c1b5980739ba4e2d50867f9ba1005d2fa4997f14
Jordan Banasik <woovie@woovie.net>
2019-11-18 20:10:20 +0000

    Updated crawl.py to do more than just GET requests

--- scraper/crawl.py
+++ scraper/crawl.py
@@ -1,11 +1,30 @@
-import aiohttp
-from sys import exit
+import aiohttp, logging
 
-async def crawl(url):
+logger = logging.getLogger('scraperMain')
+
+valid_methods = [
+        'POST',
+        'PATCH',
+        'PUT'
+        'GET',
+        'DELETE',
+        'OPTIONS',
+        'HEAD'
+]
+
+async def crawl(url, request = None, payload = None, json = None):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as r:
-            if r.status == 200:
-                return await r.text()
-            else:
-                print(f"[ERROR] GET {url} returned unexpected value: {r.status} {r.reason}, expected 200 OK")
-                exit()
+        if request in valid_methods or not request:
+            method = request if request else 'GET'
+            async with session.request(method, url, data = payload, json = json) as r:
+                return await process_return(r)
+        else:
+            logger.error(f"Request type {request} is invalid.")
+            return False
+
+async def process_return(request_object):
+    if request_object.status < 400:#Normal
+        return await request_object.text()
+    else:
+        logger.error(f"{request_object.url} returned {request_object.status} {request_object.reason}")
+        return False
