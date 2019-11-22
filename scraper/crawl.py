39cfe5b76b250845173cbaa148685b888e4a915a
Jordan Banasik <woovie@woovie.net>
2019-11-22 21:27:21 +0000

    Minor changes to crawl and elasticsearch

--- scraper/crawl.py
+++ scraper/crawl.py
@@ -5,26 +5,24 @@ logger = logging.getLogger('scraperMain')
 valid_methods = [
         'POST',
         'PATCH',
-        'PUT'
+        'PUT',
         'GET',
         'DELETE',
         'OPTIONS',
         'HEAD'
 ]
 
-async def crawl(url, request = None, payload = None, json = None):
+async def crawl(url, method = None, payload = None, json = None):
     async with aiohttp.ClientSession() as session:
-        if request in valid_methods or not request:
-            method = request if request else 'GET'
+        if method in valid_methods or not method:
+            method = method if method else 'GET'
             async with session.request(method, url, data = payload, json = json) as r:
                 return await process_return(r)
         else:
-            logger.error(f"Request type {request} is invalid.")
             return False
 
 async def process_return(request_object):
     if request_object.status < 400:#Normal
         return await request_object.text()
     else:
-        logger.error(f"{request_object.url} returned {request_object.status} {request_object.reason}")
-        return False
+        return await request_object.text()
--- scraper/elasticsearch.py
+++ scraper/elasticsearch.py
@@ -12,7 +12,7 @@ class ElasticSearch():
     def __init__(self, url):
         self.url = url
     async def list(self, endpoint = None):
-        url = f"{self.url}{endpoint}"
+        url = f"{self.url}{endpoint or ''}"
         return await crawl.crawl(url)
     async def store(self, endpoint, data):
         url = f"{self.url}{endpoint}"
