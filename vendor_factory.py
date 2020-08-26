01fb338e1c4d88bce8b7f103d3473b7b3f0ca038
Jordan Banasik <woovie@woovie.net>
2020-08-26 00:29:07 -0700

    Created vendor factory, vendor data
    The vendor factory will be used to initialize vendor objects for usage in other tools
    of this codebase
    
    Vendor data contains a basic starting list of vendors to scrape, who are
    all on Shopify.

---
+++ vendor_factory.py
@@ -0,0 +1,20 @@
+import json
+import scraper.vendor as vendor
+import scraper.shopify as shopify
+
+class Vendors():
+    def __init__(self, vendors_file: str):
+        self.vendors_file = vendors_file
+        self.vendors = []
+
+    def load(self):
+        with open(self.vendors_file, "r") as json_data:
+            self.vendors = json.load(json_data)
+
+    def build_vendors(self):
+        for vendor in self.vendors:
+            self.build_vendor(vendor)
+
+    def build_vendor(self, vendor_dict: dict) -> vendor.Vendor:
+        if vendor_dict["cms"] == "Shopify":
+            return shopify.ShopifyVendor(vendor_dict)
